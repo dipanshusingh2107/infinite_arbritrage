4c991a5360e1104f6e8e3c9373e5a2da6a24f083
Sushant Kumar <somi@Somis-MacBook-Air.local>
2020-08-17 00:57:44 +0530

    made a map nothing is working

--- javascript.js
+++ javascript.js
@@ -1,52 +1,91 @@
+
+function map_making()
+{
+    var map=new Map();
+    map.set([1,2],10);
+    map.set([1,3],7);
+    map.set([3,6],7);
+    map.set([6,8],15);
+    map.set([8,9],5);
+    map.set([9,7],9);
+    map.set([7,4],9);
+    map.set([4,2],15);
+    map.set([2,6],12);
+    map.set([6,7],22);
+    map.set([5,2],10);
+    map.set([5,3],15);
+    map.set([5,8],8);
+    map.set([5,7],12);
+    map.set([4,8],13);
+
+
+
+    return map;
+}
+
+
+
+
 function path(parent,dest)
 {
-    var stack =new stack();
+    var stack=[];
     stack.push(dest);
     var i=dest;
-    while(i!= -1)
+
+    while(i != -1)
     {
         i=parent[i];
         stack.push(i);
     }
     stack.pop();
 
-    var answer="Shortest Path ";
-    var 
+    var answer="Shortest_Path ";
     while(!stack.isEmpty()){
 
-        answer =answer+(stack.peek()+1)+" ->";
+        answer =answer+stack.peek()+" ->";
         stack.pop();
     }
-    document.querySelector('answer').innerHTML=answer;
+    console.log(answer);
+    document.querySelector('#answer').innerHTML=answer;
+
 }
+
+
+
 
 function shortest_path(graph,src,dest)
 {
-    var distance=new array();
+    
+    var distance=[];
 
     for(i=0;i<=9;i++)
     distance.push(9999999);
 
     distance[src]=0;
     
-    var parent = new array();
+    var parent = [];
     parent[src]= -1;
 
+    //var length=[0,2,4,3,3,4,4,4,4,2];
+
+    var map= map_making();
+    
     for(i=1;i<9;i++)
     {
-        for(j=0;j<9;j++)
+        for(j=1;j<=9;j++)
         {
-            for(k=0;k<graph[j].size();k++)
+            for(k=0;k<graph[j].length;k++)
             {
-                if(distance[graph[j][k][0]]> distance[j]+graph[j][k][1])
+                if(distance[graph[j][k]]> distance[j]+map.get([j,k]))
                 {
-                    distance[graph[j][k][0]]=distance[j]+graph[j][k][1];
-                    parent[graph[j][k][0]]=j;
+                    distance[graph[j][k]]=distance[j]+map.get([j,k]);
+                    parent[graph[j][k]]=j;
                 }
             }
         }
     }
-path(parent,dest);
+    
+    console.log(distance[dest]);
         
 }
 
@@ -56,22 +95,26 @@ path(parent,dest);
 function main()
 {
     var graph= [
-        [ [2,10] , [3,7] ],
-        [ [1],[5],[4],[6] ] ,
-        [ [1],[5],[6] ] ,
-        [ [2],[7],[8] ] ,
-        [ [2],[3],[7],[8] ] ,
-        [ [2],[3],[7],[8] ] ,
-        [ [4],[5],[6],[9] ] ,
-        [ [4],[5],[6],[9] ] ,
-        [ [7],[8] ]
+        ["This is here to make indexing one based"]
+        [2,3],
+        [1,5,4,6] ,
+        [1,5,6] ,
+        [2,7,8] ,
+        [2,3,7,8] ,
+        [2,3,7,8] ,
+        [4,5,6,9] ,
+        [4,5,6,9] ,
+        [7,8]
     ];
     var e=15;
     var n=9;
 
-    var src=document.querySelector("#input_source").value;
-    var dest=document.querySelector("#input_destination").value;
-   shortest_path(graph,src,dest);
+    var src= (document.querySelector("#input_source").value);
+    var dest= (document.querySelector("#input_destination").value);
+
+    console.log(graph[2][2]);
+   
+    shortest_path(graph,src,dest);
 
 }
 
